f592a3e5f0f5f4d7973dae33e1d68d8d90ef635c
MarcoLomolino <marco31998@gmail.com>
2021-01-16 21:45:00 +0100

    Corretto il bug dove avevo invertito i box 2 e 3

--- app/src/main/java/com/example/drawabletest/Highscores.java
+++ app/src/main/java/com/example/drawabletest/Highscores.java
@@ -4,10 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.TextView;
 
