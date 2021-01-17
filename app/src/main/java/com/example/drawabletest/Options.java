6c2d514ba2ed393e62697f2ebad5cb4e3fb62067
Gatetano <gaecass@live.com>
2021-01-17 13:01:22 +0100

    cazzata fix

--- app/src/main/java/com/example/drawabletest/Options.java
+++ app/src/main/java/com/example/drawabletest/Options.java
@@ -4,8 +4,12 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.Toast;
@@ -14,6 +18,8 @@ public class Options extends AppCompatActivity {
 
     private CheckBox difficulty_classic;
     private CheckBox difficulty_Hard;
+    private SharedPreferences mPreferences;
+    private SharedPreferences.Editor mEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,22 +30,35 @@ public class Options extends AppCompatActivity {
         ab.setDisplayHomeAsUpEnabled(true);
 
 
-        difficulty_classic = findViewById(R.id.checkBox);
-        difficulty_Hard = findViewById(R.id.checkBox2);
+        difficulty_classic = findViewById(R.id.checkBox_classic);
+        difficulty_Hard = findViewById(R.id.checkBox_hard);
 
+        mPreferences = getSharedPreferences("com.example.drawabletest", Context.MODE_PRIVATE);
+        mEditor = mPreferences.edit();
+
+        mPreferences = getSharedPreferences("com.example.drawabletest", Context.MODE_PRIVATE);
+        String difficulty = mPreferences.getString("difficulty", "classic");
+        if (difficulty.equals("classic")) {
+            difficulty_classic.setChecked(true);
+            difficulty_classic.setClickable(false);
+        } else {
+            difficulty_Hard.setChecked(true);
+            difficulty_Hard.setClickable(false);
+        }
 
        difficulty_Hard.setOnCheckedChangeListener((CompoundButton.OnCheckedChangeListener) (group, checkedId) -> {
             if(difficulty_Hard.isChecked())
                 difficulty_classic.setChecked(false);
-            else
-                difficulty_classic.setChecked(true);
+                difficulty_Hard.setClickable(false);
+                difficulty_classic.setClickable(true);
        });
 
         difficulty_classic.setOnCheckedChangeListener((CompoundButton.OnCheckedChangeListener) (group, checkedId) -> {
             if(difficulty_classic.isChecked())
                 difficulty_Hard.setChecked(false);
-            else
-                difficulty_Hard.setChecked(true);
+                difficulty_classic.setClickable(false);
+                difficulty_Hard.setClickable(true);
+
         });
     }
 
@@ -49,8 +68,31 @@ public class Options extends AppCompatActivity {
                 Toast.makeText(this, "PRESO", Toast.LENGTH_LONG);
                 finish();
                 return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
         }
-        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    public void onDifficultyClicked(View view) {
+        boolean checked = ((CheckBox) view).isChecked();
+        switch(view.getId()) {
+            case R.id.checkBox_classic:
+                if (checked) {
+                    Toast.makeText(this, "Classic Difficulty selected", Toast.LENGTH_SHORT).show();
+                    mEditor.putString("difficulty","classic");
+                    mEditor.commit();
+                }
+                break;
+            case R.id.checkBox_hard:
+                if (checked) {
+                    Toast.makeText(this, "Hard Difficulty selected", Toast.LENGTH_SHORT).show();
+                    mEditor.putString("difficulty","difficult");
+                    mEditor.commit();
+                }
+                break;
+        }
     }
 
 
