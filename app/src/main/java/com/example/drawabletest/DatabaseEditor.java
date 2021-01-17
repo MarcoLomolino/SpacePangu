761ccaa869f1d6f3727cfdfda9a4a19e89b8eab5
leaca <lea.cami17@gmail.com>
2021-01-17 18:32:42 +0100

    Fix del problema del database

--- app/src/main/java/com/example/drawabletest/DatabaseEditor.java
+++ app/src/main/java/com/example/drawabletest/DatabaseEditor.java
@@ -3,6 +3,7 @@ package com.example.drawabletest;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -13,10 +14,10 @@ import java.util.Collections;
 import java.util.List;
 
 public class DatabaseEditor extends SQLiteOpenHelper {
-    public static final String CUSTOMER_TABLE = "TABELLA";
-    public static final String X_COLUMN = "X";
-    public static final String Y_COLUMN = "Y";
-    public static final String ATTIVO_COLUMN = "ATTIVO";
+    private static final String EDITOR = "EDITOR";
+    private static final String X_COLUMN = "X";
+    private static final String Y_COLUMN = "Y";
+    private static final String ATTIVO_COLUMN = "ATTIVO";
 
     public DatabaseEditor(@Nullable Context context) {
         super(context, "customer.db", null, 1);
@@ -24,13 +25,18 @@ public class DatabaseEditor extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String createTableStatement = "CREATE TABLE " + CUSTOMER_TABLE + " (LIVELLO INT, " + X_COLUMN + " INT, " + Y_COLUMN + " INT, " + ATTIVO_COLUMN+ " INT, PRIMARY KEY(LIVELLO,X,Y))";
-        db.execSQL(createTableStatement);
+        try{
+            String createTableStatement = "CREATE TABLE " + EDITOR + " (LIVELLO INT, " + X_COLUMN + " INT, " + Y_COLUMN + " INT, " + ATTIVO_COLUMN+ " INT, PRIMARY KEY(LIVELLO,X,Y))";
+            db.execSQL(createTableStatement);
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
 
         for(int z=1;z<5;z++){
             for(int x=0;x<5;x++){
                 for(int y=0;y<4;y++){
-                    db.execSQL("INSERT INTO "+ CUSTOMER_TABLE+ " VALUES("+z+","+x+","+y+",0)");
+                    db.execSQL("INSERT INTO "+ EDITOR+ " VALUES("+z+","+x+","+y+",0)");
                 }
             }
         }
@@ -41,15 +47,17 @@ public class DatabaseEditor extends SQLiteOpenHelper {
 
     }
 
+
+
     public void modificaBlocchi(int x, int y, int attivo, int livello){
         SQLiteDatabase db = this.getWritableDatabase();
-        db.execSQL("UPDATE TABELLA SET ATTIVO="+attivo +" WHERE X="+x+" AND Y="+y+" AND LIVELLO="+livello);
+        db.execSQL("UPDATE EDITOR SET ATTIVO="+attivo +" WHERE X="+x+" AND Y="+y+" AND LIVELLO="+livello);
     }
 
     public List<ModelEditor> getRecord(int livello){
         List<ModelEditor> list = new ArrayList<>();
 
-        String queryString = "SELECT * FROM " + CUSTOMER_TABLE+ " WHERE LIVELLO="+livello;
+        String queryString = "SELECT * FROM " + EDITOR+ " WHERE LIVELLO="+livello;
         SQLiteDatabase db = this.getReadableDatabase();
         Cursor cursor = db.rawQuery(queryString, null);
         if(cursor.moveToFirst()){
